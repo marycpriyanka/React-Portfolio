0e9a4b0fbe3af5923cc45b98717ec9e76fab3cda
marycpriyanka <marycpriyanka@gmail.com>
2022-05-10 18:32:11 -0700

    Add About me component

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import "./css/App.css";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About></About>
       <Footer />
     </div>
   );
---
+++ client/src/components/About.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import $ from "jquery";
+
+export default function Header() {
+    const interval = setInterval(animatePhoto, 7000);
+
+    // Adds a little animation
+    function animatePhoto() {
+        // Shakes the photo vertically
+        $("#priyanka").effect("bounce", { times: 3 }, "slow");
+    }
+
+    return (
+        <section id="about" class="section">
+            <h2>About me</h2>
+            <div>
+                <figure>
+                    <img id="priyanka" src="images/Priyanka.jpg" alt="Priyanka Mary Christine"></img>
+                </figure>
+                <p>
+                    A full stack web developer based in Seattle. Passionate about building software that is meaningful and can improve our lives. 
+                    Have a degree in Electronics and Communication Engineering and 5 years of previous experience as C# .net developer. 
+                    Focused on writing neat, organized and quality code thereby optimizing efficiency.  Recognized for conducting technical interviews to add valuable members 
+                    to our team. Recently earned a certificate in full stack web development from the University of Washington, from which I gained knowledge and 
+                    critical skills needed to build dynamic web applications and deploy them. Have worked on technologies like HTML5, CSS3, JavaScript, 
+                    Node.js, MySQL, MongoDB, Heroku, Express.js, React.js. I am eager to utilize my technical skills and my strengths like problem solving, 
+                    perseverance, planning and organization, and the ability to work well in a diverse team environment on every project I work on.
+                </p>
+            </div>
+        </section>
+    );
+}
