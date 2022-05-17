a6a33dc543ed44065cdbebe81334214f3a0100e1
marycpriyanka <marycpriyanka@gmail.com>
2022-05-17 14:05:26 -0700

    Add components ProjectOdd and ProjectEven

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ import About from "./pages/About"
 import Contact from "./pages/Contact";
 import Work  from "./pages/Work";
 
+// Imports list of projects from project.js
+import projects from "./projects";
+
 function App() {
   return (
     <Router>
@@ -16,7 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={<About />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/work" element={<Work />}></Route>
+          <Route path="/work" element={<Work projects={projects}/>}></Route>
         </Routes>
         <Footer />
       </div>
---
+++ client/src/projects.js
@@ -0,0 +1,49 @@
+const projects = [
+    {
+        id: 1,
+        title: "Quiz-Hub",
+        image: "/images/projects/quiz_hub.jpg",
+        description: "Quiz hub is a user login quiz website to allow people to share their knowledge of topics with other people by putting them to the test. It could be used by an online trivia enthusiast to post quizzes and to take quizzes that others have posted so that they could test and compare their knowledge with other trivia enthusiasts.",
+        technologies: "Node.js | Express.js | mysql | sequelize | handlebars | CSS | JavaScript",
+        demoUrl: "https://quiz-hub-2022.herokuapp.com/",
+        githubUrl: "https://github.com/marycpriyanka/quiz-hub"
+    },
+    {
+        id: 2,
+        title: "Cal-It",
+        image: "/images/projects/Calit.JPG",
+        description: " Cal-it is a recipe search website where users can search for recipes based on like mealtype, diet, main ingredient and cooking time.",
+        technologies: "HTML | CSS | Bootstrap | JavaScript | AJAX | Responsive Design",
+        demoUrl: "https://marycpriyanka.github.io/recipe-recommendation/",
+        githubUrl: "https://github.com/marycpriyanka/recipe-recommendation"
+    },
+    {
+        id: 3,
+        title: "Weather dashboard",
+        image: "/images/projects/WeatherDashboard.JPG",
+        description: "Weather dashboard retrieves weather data for cities.",
+        technologies: "HTML | CSS | Bootstrap | JavaScript | Open Weather One Call API",
+        demoUrl: "https://marycpriyanka.github.io/Weather-dashboard/",
+        githubUrl: "https://github.com/marycpriyanka/Weather-dashboard"
+    },
+    {
+        id: 4,
+        title: "Tech blog",
+        image: "/images/projects/tech_blog.JPG",
+        description: "A blog site where developers can publish their blog posts and comment on other developer's posts as well.",
+        technologies: "Node.js | Express.js | Handlebars | MySql | Sequelize | Heroku",
+        demoUrl: "https://blooming-hamlet-94670.herokuapp.com/",
+        githubUrl: "https://github.com/marycpriyanka/tech-blog"
+    },
+    {
+        id: 5,
+        title: "Note Taker",
+        image: "/images/projects/note_taker.JPG",
+        description: "Note Taker is an application used to write, save and delete notes. This is very useful to all types of users like small business owners, so that they can organize their thoughts and keep track of tasks they need to complete.",
+        technologies: "Node.js | Express.js | HTML",
+        demoUrl: "https://pacific-caverns-10678.herokuapp.com/",
+        githubUrl: "https://github.com/marycpriyanka/note-taker"
+    },
+];
+
+export default projects;
