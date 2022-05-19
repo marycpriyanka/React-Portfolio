dd5b16c44aff53ddd6ae278aa316bcfae7b076e2
marycpriyanka <marycpriyanka@gmail.com>
2022-05-18 22:03:36 -0700

    Animate skills

--- client/src/data/skills.js
+++ client/src/data/skills.js
@@ -1,7 +1,7 @@
 const skills = [
     "HTML5" , "CSS3", "JavaScript", "jQuery", "Bootstrap", 
     "Responsive Design", "AJAX", "Node.js"," Express.js", "MySQL", "MongoDB", "Sequelize",
-    "Mongoose", "Handlebars.js", "REact.js", "C#", "Git", "Heroku", "MVC", 
+    "Mongoose", "Handlebars.js", "React.js", "C#", "Git", "Heroku", "MVC", 
 ];
 
 export default skills;
--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import $ from "jquery";
 
 export default function Resume({ skills }) {
+    useEffect(() => {
+        let toggleSize = true;
+
+        const interval = setInterval(animateSkills, 6000);
+
+        // Adds a little animation
+        function animateSkills() {
+            if (toggleSize) {
+                $(".skill").animate({ "padding": "1.5%" }, "slow");
+                toggleSize = false;
+            }
+            else {
+                $(".skill").animate({ "padding": "1%" }, "slow");
+                toggleSize = true;
+            }
+        }
+    }, [])
+
     return (
         <section className="section">
-            <h2>Skills</h2>
             <h3><a id="resume" href="https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:e64c6ad2-93d3-4e8b-861b-5124d57dbc7b"
-                target="_blank" rel="noreferrer">View my Resume</a></h3>
+                target="_blank" rel="noreferrer">Download my Resume</a></h3>
+            <h2>Skills</h2>
             <div id="skillDiv">
-                {skills.map(skill => <span id="skill">{skill}</span>)}
+                {skills.map(skill => <span className="skill">{skill}</span>)}
             </div>
         </section>
     );
