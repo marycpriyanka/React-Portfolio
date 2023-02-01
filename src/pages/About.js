c606ddfb804f3f7942b5893164ee7da9486f1127
marycpriyanka <marycpriyanka@gmail.com>
2023-02-01 12:57:06 -0800

    Modified introduction

--- src/pages/About.js
+++ src/pages/About.js
@@ -30,12 +30,12 @@ export default function Header() {
                     <img id="priyanka" src="images/Priyanka.jpg" alt="Priyanka Mary Christine"></img>
                 </figure>
                 <p>
-                    A full stack web developer based in Seattle. Passionate about building software that is meaningful and can improve our lives.
+                     A full stack web developer based in Seattle with a passion for building software that is meaningful and can improve our lives.
                     Have a degree in Electronics and Communication Engineering and 5 years of previous experience as C# .net developer.
                     Focused on writing neat, organized and quality code thereby optimizing efficiency.  Recognized for conducting technical interviews to add valuable members
                     to our team. Recently earned a certificate in full stack web development from the University of Washington, from which I gained knowledge and
                     critical skills needed to build dynamic web applications and deploy them. Have worked on technologies like HTML5, CSS3, JavaScript,
-                    Node.js, MySQL, MongoDB, Heroku, Express.js, React.js. I am eager to utilize my technical skills and my strengths like problem solving,
+                    Node.js, MySQL, MongoDB, Heroku, Express.js, React.js. Also earned a certificate in Full Stack Java from Southern Methodist University. Currently working as Teaching Assistant for edX in Full Stack Web Development. I am eager to utilize my technical skills and my strengths like problem solving,
                     perseverance, planning and organization, and the ability to work well in a diverse team environment on every project I work on.
                 </p>
             </div>
